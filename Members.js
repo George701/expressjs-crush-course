2a044a8b68199de61590e16c4bf5fdd554057fb2
George701 <chernikov.georgii@gmail.com>
2019-08-21 20:58:53 +0300

    Initial commit

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Karen Smith',
+        email: 'karen.smith@gmail.com',
+        status: 'inactive'
+    },
+];
+
+module.exports = members;
